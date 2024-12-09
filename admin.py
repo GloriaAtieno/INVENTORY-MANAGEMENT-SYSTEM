370924e502c896a07e4b92765efb5744d1b36dc9
GloriaAtieno <atienogloria07@gmail.com>
2024-12-09 19:11:45 +0300

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from lmsApp import models
+from lmsApp.models import Category, SubCategory, Books, Students, Borrow
+
+# Register your models here.
+# admin.site.register(models.Groups)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Books)
+admin.site.register(Students)
+admin.site.register(Borrow)
